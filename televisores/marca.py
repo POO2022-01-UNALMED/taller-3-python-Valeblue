19258f150214e2757a98c24817a2b3f50cf52612
Valeblue <101608411+Valeblue@users.noreply.github.com>
2022-03-29 22:41:13 -0500

    :rage3:
    Py3

---
+++ televisores/marca.py
@@ -0,0 +1,14 @@
+from ast import Return
+
+
+class Marca:
+    def __init__(self,nombre):
+        self.nombre=nombre
+        def etNombre(self,nombre):
+            self.nombre=nombre
+            def getNombre(self):
+                return self.nombre = nombre
+                
+
+
+
